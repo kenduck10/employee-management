ea24bf888a8ec02a5d6ba520ddca46cbce04490e
kentaro_kami <kentaro_kami@kenduck.com>
2025-06-25 23:58:05 +0900

    mybatis config追加

--- backend/common/src/main/java/com/kenduck10/common/CommonApplication.java
+++ backend/common/src/main/java/com/kenduck10/common/CommonApplication.java
@@ -1,11 +1,11 @@
-package com.example.employee.common;
+package com.kenduck10.common;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommonApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CommonApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(CommonApplication.class, args);
+  }
 }
