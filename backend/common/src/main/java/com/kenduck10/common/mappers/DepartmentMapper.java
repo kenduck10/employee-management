eaf552fca3643c6be05d839323a95750c604c1cb
kentaro_kami <kentaro_kami@kenduck.com>
2025-06-29 18:25:37 +0900

    mapper整理

--- backend/common/src/main/java/com/kenduck10/common/mappers/DepartmentMapper.java
+++ backend/common/src/main/java/com/kenduck10/common/mappers/DepartmentMapper.java
@@ -1,5 +1,32 @@
 package com.kenduck10.common.mappers;
 
-public class DepartmentMapper extends GeneratedDepartmentMapper {
+import static com.kenduck10.common.mappers.generated.GeneratedDepartmentDynamicSqlSupport.code;
+import static org.mybatis.dynamic.sql.SqlBuilder.isIn;
+import java.util.List;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.BeanUtils;
+import com.kenduck10.common.mappers.generated.GeneratedDepartmentMapper;
+import com.kenduck10.common.models.Department;
 
+public interface DepartmentMapper extends GeneratedDepartmentMapper {
+
+  /**
+   * 指定されたコードリストに含まれるコードを持つ部署を取得します。
+   *
+   * @param codes 部署コードのリスト
+   * @return 指定されたコードリストに含まれるコードを持つ部署のリスト
+   * @throws IllegalArgumentException コードリストが空の場合
+   */
+  default List<Department> selectByCodes(List<String> codes) {
+    if (CollectionUtils.isEmpty(codes)) {
+      throw new IllegalArgumentException("Code list cannot be empty");
+    }
+    return select(c -> c.where(code, isIn(codes))).stream()
+        .map(generatedDepartment -> {
+          Department department = new Department();
+          BeanUtils.copyProperties(generatedDepartment, department);
+          return department;
+        })
+        .toList();
+  }
 }
