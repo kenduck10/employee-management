5d3c517935609071bf3b6c0b81d7bc8ba0de40a4
kentaro_kami <kentaro_kami@kenduck.com>
2025-06-29 17:32:06 +0900

    javadoc追記

--- backend/common/src/main/java/com/kenduck10/common/models/Department.java
+++ backend/common/src/main/java/com/kenduck10/common/models/Department.java
@@ -4,11 +4,26 @@ import com.kenduck10.common.models.generated.GeneratedDepartment;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+/**
+ * 部署エンティティを表すモデルクラス。
+ * 
+ * このクラスは自動生成された {@link GeneratedDepartment} クラスを継承し、 部署エンティティに対する追加のロジックと定数を提供します。
+ * 部署には部署コード、部署名、説明などの情報が含まれます。
+ * 
+ * @see GeneratedDepartment
+ */
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Department extends GeneratedDepartment {
 
+  /**
+   * 部署コードの最大許可長。
+   */
   public static final int CODE_MAX_LENGTH = 10;
+
+  /**
+   * 部署名の最大許可長。
+   */
   public static final int NAME_MAX_LENGTH = 100;
 
 }
